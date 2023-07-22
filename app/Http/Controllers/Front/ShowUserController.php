454e8d68841024716685d846eba5c362c7732fa3
unknown <Alizaher88.azln3820172gmail.com>
2023-07-22 05:32:28 -0700

    first commit

---
+++ app/Http/Controllers/Front/ShowUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class ShowUserController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+    public function index()
+    {
+
+       return view('ecommercehome');
+
+    }
+
+}
